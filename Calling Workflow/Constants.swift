938b367daca54ba14705cc0e19b3058961e975e7
Matt Stauffer <matt.stauffer@ldschurch.org>
2017-04-21 11:30:19 -0600

    removed google oauth constants that are not needed with new google sign-in

--- Calling Workflow/Constants.swift
+++ Calling Workflow/Constants.swift
@@ -32,12 +32,6 @@ struct NetworkConstants {
 struct RemoteStorageConstants {
     // for right now we have a single file. Eventually will likely have a file per sub org.
     static let dataFileName = "cwf-unit-data.json"
-    // taken from google developers console. This is particular to local dev machine. When we approach shipping will
-    // need to get a prod level one
-    static let oauthClientId = "***REMOVED***"
-    // This is just a name to use as a key to store the google credentials in the keychain, can be anything just needs to 
-    // be the same between writes to & reads from keychain
-    static let authTokenKeychainId = "cwf-google-oauth"
     
     static let dataFileMimeType = "application/json"
     // special Application Data folder available in google drive that is not viewable by users outside the app that created it
