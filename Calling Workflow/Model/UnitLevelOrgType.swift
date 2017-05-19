7cccf495efc99e75709b71ae3e536384547e5919
Matt Stauffer <matt.stauffer@ldschurch.org>
2017-05-19 14:18:48 -0600

    Updated Branch structure differences

--- Calling Workflow/Model/UnitLevelOrgType.swift
+++ Calling Workflow/Model/UnitLevelOrgType.swift
@@ -27,11 +27,12 @@ public enum UnitLevelOrgType : Int  {
     case SundaySchool = 75
     case Primary = 77
     case WardMissionaries = 1310
+    case BranchMissionaries = 1283
     case Other = 1185
     
     static let wardOrgTypes = [Bishopric, HighPriests, Elders, ReliefSociety, YoungMen, YoungWomen, SundaySchool, Primary, WardMissionaries, Other]
     
-    static let branchOrgTypes = [BranchPresidency, HighPriests, Elders, ReliefSociety, YoungMen, YoungWomen, SundaySchool, Primary, Other]
+    static let branchOrgTypes = [BranchPresidency, HighPriests, Elders, ReliefSociety, YoungMen, YoungWomen, SundaySchool, Primary, BranchMissionaries, Other]
     
 }
 
