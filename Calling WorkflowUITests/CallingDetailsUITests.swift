fdc8a71a956e06c21f1de60d009823e13d146189
Chad Olsen <chadolsen@Chads-MacBook-Pro.local>
2017-09-07 10:00:07 -0600

    updates to lcr calling updates.

--- Calling WorkflowUITests/CallingDetailsUITests.swift
+++ Calling WorkflowUITests/CallingDetailsUITests.swift
@@ -20,9 +20,10 @@ class CallingDetailsUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
         
-        
+
         
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
     }
     
     override func tearDown() {
@@ -30,12 +31,12 @@ class CallingDetailsUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testSomething() {
+        
         let tablesQuery = XCUIApplication().tables
-        tablesQuery.staticTexts["High Priests Group"].tap()
-        tablesQuery.staticTexts["High Priests Group Leadership"].tap()
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        tablesQuery.staticTexts["Elders Quorum"].tap()
+        tablesQuery.staticTexts["Elders Quorum Presidency"].tap()
+        tablesQuery.staticTexts["Family5, Husband (9 months)"].tap()
+        //XCTAssertEqual(, <#T##expression2: [Equatable]##[Equatable]#>)
     }
-    
 }
